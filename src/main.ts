e24d256a062a2002de85fd2917c34dff49242cdb
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-09-09 21:42:39 -0500

    feat: use env variables for configuration

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3001;
+  const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000';
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: clientUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3001);
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
